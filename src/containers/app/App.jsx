d6fbff145fcf84bc157f94adafd6894de9e0910c
b3m3 <uwm.d.c.a@gmail.com>
2023-01-04 14:10:26 +0200

    Movies lib

---
+++ src/containers/app/App.jsx
@@ -0,0 +1,15 @@
+import './app.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header></header>
+      <div className='app-wrapp'>
+        <aside></aside>
+        <main></main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
