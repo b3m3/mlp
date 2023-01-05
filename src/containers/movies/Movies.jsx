293a2125360db1df9859212c595221af0273e207
b3m3 <uwm.d.c.a@gmail.com>
2023-01-05 14:40:59 +0200

    Movies.jsx

---
+++ src/containers/movies/Movies.jsx
@@ -0,0 +1,23 @@
+import Preview from '../../components/preview/Preview';
+import { MOVIES_POPULAR, MOVIES_TOP_RATED, MOVIES_NOW_PLAYING, MOVIES_UPCOMING } from '../../constans/api';
+
+import style from './movies.module.scss';
+
+const Movies = () => {
+  const items = [
+    {title: 'Popular', url: MOVIES_POPULAR},
+    {title: 'Top rated', url: MOVIES_TOP_RATED},
+    {title: 'Now playing', url: MOVIES_NOW_PLAYING},
+    {title: 'Upcoming', url: MOVIES_UPCOMING}
+  ];
+
+  return (
+    <section className={style.movies}>
+      {items.map((props, i) => (
+        <Preview key={i} {...props}/>
+      ))}
+    </section>
+  );
+}
+
+export default Movies;
