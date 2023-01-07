1a76e3b9fdbcb2a13e73e1852d4f57920f24951a
b3m3 <uwm.d.c.a@gmail.com>
2023-01-07 13:27:07 +0200

    create Category route path

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,13 +2,17 @@ import Home from "../containers/home/Home";
 import Movies from '../containers/movies/Movies';
 import Serials from '../containers/serials/Serials';
 import Favorites from '../containers/favorites/Favorites';
+import Category from "../containers/category/Category";
 import NotFound from '../containers/notFound/NotFound';
+
+import { API_LANGUAGE, API_PAGE } from "../constans/api";
 
 const routes = [
   { path: '/', element: <Home /> },
-  { path: '/movies', element: <Movies /> },
-  { path: '/serials', element: <Serials /> },
+  { path: '/movie', element: <Movies /> },
+  { path: '/tv', element: <Serials /> },
   { path: '/favorites', element: <Favorites /> },
+  { path: '/:video'+'/:category'+API_LANGUAGE+':lang'+API_PAGE+':pageid', element: <Category /> },
   { path: '*', element: <NotFound /> }
 ];
 
