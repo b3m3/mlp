dacb3b0261ba41868b620614fe2a65c79b82ac04
b3m3 <uwm.d.c.a@gmail.com>
2023-01-07 18:07:09 +0200

    func setLanguagePathname

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -5,3 +5,14 @@ export const textOverflow = (text, length) => {
 export const titleToLocation = str => {
   return '/' + str.toLowerCase().split(' ').join('_');
 };
+
+export const setLanguagePathname = (pathname, lang) => {
+  const find = 'lang=';
+  const index = pathname.indexOf(find);
+
+  if (index === -1) {
+    return;
+  } else {
+    return pathname.slice(0, index + find.length) + lang;
+  }
+};
