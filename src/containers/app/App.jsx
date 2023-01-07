f8421ca588f5f1bbd2d67d40c925a2e1c254752e
b3m3 <uwm.d.c.a@gmail.com>
2023-01-07 13:33:20 +0200

    fix Context.Provider languages

--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -1,39 +1,48 @@
 import { useState } from 'react';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from '../../components/header/Header';
 import Sidebar from '../../components/sidebar/Sidebar';
 import routes from '../../routes/routes';
 
+import { Context } from '../../context/context';
+
 import './app.scss';
 
 const App = () => {
   const [menuActive, setMenuActive] = useState(false);
+  const [currentLang, setCurrentLang] = useState('EN');
+
+  const languages = ['EN', 'RU'];
 
   return (
-    <BrowserRouter>
-    <div className="app">
-      <Header 
-        onClick={() => setMenuActive(a => !a)}
-        menuActive={menuActive}
-      />
-      <div className="container">
-        <div className='app-wrapp'>
-          <Sidebar 
+    <Context.Provider value={{
+      currentLang,
+      setCurrentLang,
+      languages
+    }}>
+      <BrowserRouter>
+        <div className="app">
+          <Header 
+            onClick={() => setMenuActive(a => !a)}
             menuActive={menuActive}
           />
-          <main>
-            <Routes>
-              {routes && routes.map(({ path, element }, i) => (
-                <Route key={i} path={path} element={element} />
-              ))}
-            </Routes>
-          </main>
+          <div className="container">
+            <div className='app-wrapp'>
+              <Sidebar 
+                menuActive={menuActive}
+              />
+              <main>
+                <Routes>
+                  {routes && routes.map(({ path, element }, i) => (
+                    <Route key={i} path={path} element={element} /> ))}
+                </Routes>
+              </main>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Context.Provider>
   );
 }
 
