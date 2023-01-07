23299caf531c1620d6adf2e5d87c6cf3bc006424
b3m3 <uwm.d.c.a@gmail.com>
2023-01-07 13:27:54 +0200

    func titleToLocation

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,7 @@
 export const textOverflow = (text, length) => {
   return text.length > length ? text.slice(0, length) + '..' : text;
 };
+
+export const titleToLocation = str => {
+  return '/' + str.toLowerCase().split(' ').join('_');
+};
