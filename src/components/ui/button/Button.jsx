7452887e07fc86679379ce07dc5b126cc35a30d3
b3m3 <uwm.d.c.a@gmail.com>
2023-01-08 21:33:11 +0200

    Button.jsx

---
+++ src/components/ui/button/Button.jsx
@@ -0,0 +1,14 @@
+import style from './button.module.scss';
+
+import {IoIosArrowDropleftCircle} from 'react-icons/io';
+import {IoIosArrowDroprightCircle} from 'react-icons/io';
+
+const Button = ({ left }) => {
+  return (
+    <button className={style.button}>
+      {left ? <IoIosArrowDropleftCircle /> : <IoIosArrowDroprightCircle />}
+    </button>
+  );
+}
+
+export default Button;
