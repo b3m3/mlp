060211ce094ed8f970912bdabe7424f3186629c2
b3m3 <uwm.d.c.a@gmail.com>
2023-01-08 12:41:32 +0200

    func translateTitles

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -6,6 +6,10 @@ export const titleToPathname = str => {
   return '/' + str.toLowerCase().split(' ').join('_');
 };
 
+export const pathnameToTitle = pathname => {
+  return pathname.split('_').join(' ')[0].toUpperCase() + pathname.split('_').join(' ').slice(1);
+};
+
 export const getVideoPathname = pathname => {
   return '/' + pathname.split('/')[1];
 };
@@ -19,4 +23,8 @@ export const setLanguagePathname = (pathname, lang) => {
   } else {
     return pathname.slice(0, index + find.length) + lang + pathname.slice(index + find.length + lang.length);
   }
+};
+
+export const translateTitles = (arr, title, lang) => {
+  return arr.map(el => el[title] && el[title][0][lang]);
 };
