6b2570ad2907a1f9a97feeec7e615372c486bee9
b3m3 <uwm.d.c.a@gmail.com>
2023-01-08 12:43:07 +0200

    fix routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,12 +6,12 @@ import Category from "../containers/category/Category";
 import NotFound from '../containers/notFound/NotFound';
 
 const routes = [
-  { path: '/movie'+'/:langPath:langCode', element: <Movies /> },
-  { path: '/tv'+'/:langPath:langCode', element: <Serials /> },
-  { path: '/favorites'+'/:langPath:langCode', element: <Favorites /> },
-  { path: '/:video'+'/:langPath:langCode'+'/:category'+'/:page:pageId', element: <Category /> },
-  { path: '*', element: <NotFound /> },
-  { path: '/:langPath:langCode', element: <Home /> }
+  { path: `/movie/:langPath:langCode`, element: <Movies /> },
+  { path: `/tv/:langPath:langCode`, element: <Serials /> },
+  { path: `/favorites/:langPath:langCode`, element: <Favorites /> },
+  { path: `/:video/:langPath:langCode/:category/:page:pageId`, element: <Category /> },
+  { path: `*`, element: <NotFound /> },
+  { path: `/:langPath:langCode`, element: <Home /> }
 ];
 
 export default routes;
