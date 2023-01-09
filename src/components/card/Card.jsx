fdfa938ed1851ffa112eb0643f05d770cc9b49b7
b3m3 <uwm.d.c.a@gmail.com>
2023-01-09 14:00:10 +0200

    Card link to Info

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,9 @@
+import { Link, useLocation } from 'react-router-dom';
+import { useContext } from 'react';
+
 import { API_POSTER } from '../../constans/api';
-import { textOverflow } from '../../utils/functions';
+import { textOverflow, getVideoPathname } from '../../utils/functions';
+import { Context } from "../../context/context";
 
 import NoPoster from './img/no-poster.webp';
 
@@ -7,11 +11,22 @@ import { AiFillStar } from 'react-icons/ai';
 
 import style from './card.module.scss';
 
-const Card = ({poster_path, title, name, vote_average}) => {
+const Card = ({id, poster_path, title, name, vote_average}) => {
+  const { pathname } = useLocation();
+  const { currentLang } = useContext(Context);
+  const { lang } = currentLang;
+
+  const link = getVideoPathname(pathname)+'/'+'lang='+lang+'/'+id;
+
   return (
     <div className={style.card}>
       <div className={style.poster}>
-        <img src={poster_path ? API_POSTER+poster_path : NoPoster} alt="Poster" />
+        <Link 
+          to={link}
+          onClick={() => console.log(id)}
+        >
+          <img src={poster_path ? API_POSTER+poster_path : NoPoster} alt="Poster" />
+        </Link>
       </div>
 
       {title && <h5>{textOverflow(title, 22)}</h5>}
