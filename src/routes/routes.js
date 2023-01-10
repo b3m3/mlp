253d6d1d2ecde45b1787151a0276514fc5f31d4a
b3m3 <uwm.d.c.a@gmail.com>
2023-01-10 17:19:48 +0200

    route '/page=' -> PAGE

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,13 +6,13 @@ import Category from "../containers/category/Category";
 import Details from "../containers/details/Details";
 import NotFound from '../containers/notFound/NotFound';
 
-import { LANG } from "../constans/api";
+import { LANG, PAGE } from "../constans/api";
 
 const routes = [
   { path: `/movie${LANG}:langId`, element: <Movies /> },
   { path: `/tv${LANG}:langId`, element: <Serials /> },
   { path: `/favorites${LANG}:langId`, element: <Favorites /> },
-  { path: `/:video${LANG}:langId/:category/page=:pageId`, element: <Category /> },
+  { path: `/:video${LANG}:langId/:category${PAGE}:pageId`, element: <Category /> },
   { path: `/:video${LANG}:langId/:id`, element: <Details /> },
   { path: `*`, element: <NotFound /> },
   { path: `${LANG}:langId`, element: <Home /> }
