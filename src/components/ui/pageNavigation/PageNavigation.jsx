6b8753e75b2e2bee72bf1fb42e18aa46137ef81c
b3m3 <uwm.d.c.a@gmail.com>
2023-01-11 23:24:40 +0200

    fix PageNavigation

--- src/components/ui/pageNavigation/PageNavigation.jsx
+++ src/components/ui/pageNavigation/PageNavigation.jsx
@@ -2,7 +2,7 @@ import { Link, useParams, useLocation } from 'react-router-dom';
 
 import Button from '../button/Button';
 
-import { changePageLocation, goToPage } from '../../../utils/functions';
+import { nextPage, prevPage, goToPage } from '../../../utils/functions';
 
 import style from './page-navigation.module.scss';
 
@@ -15,35 +15,35 @@ const PageNavigation = ({ totalPages }) => {
 
   return (
     <div className={style.wrapp}>
-      <Link to={''}>
+      <Link to={pageId > 1 && prevPage(pageId, pathname)}>
         <Button left />
       </Link>
 
       <ul>
         <li>
-          <Link to={''}>1</Link>
+          <Link to={goToPage(pageId, pathname, 1)}>1</Link>
         </li>
         <li>...</li>
         <li>
-          <Link to={''}>
+          <Link to={goToPage(pageId, pathname, link_prev)}>
             {link_prev}
           </Link>
         </li>
         <li>{pageId}</li>
         <li>
-          <Link to={''}>
+          <Link to={goToPage(pageId, pathname, link_next)}>
             {link_next}
           </Link>
         </li>
         <li>...</li>
         <li>
-          <Link to={''}>
+          <Link to={goToPage(pageId, pathname, totalPages)}>
             {totalPages && totalPages}
           </Link>
         </li>
       </ul>
 
-      <Link to={''}
+      <Link to={pageId < totalPages && nextPage(pageId, pathname)}
       >
         <Button />
       </Link>
