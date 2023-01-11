28b3ea7c133a07ad8d75b4d12101c6b7183447b5
b3m3 <uwm.d.c.a@gmail.com>
2023-01-11 22:55:54 +0200

    fix SeeAll

--- src/components/ui/seeAll/SeeAll.jsx
+++ src/components/ui/seeAll/SeeAll.jsx
@@ -1,8 +1,8 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import { useContext } from 'react';
 
 import { Context } from '../../../context/context';
-import { titleToPathname, getVideoPathname } from '../../../utils/functions';
+import { titleToPathname, getVideoFromLocation } from '../../../utils/functions';
 
 import style from './see-all.module.scss';
 
@@ -10,11 +10,11 @@ const SeeAll = ({ category }) => {
   const { pathname } = useLocation();
   const { currentLang } = useContext(Context);
 
-  // const buttonNames = [{en: 'See all'}, {ru: 'Смотреть все'}, {ua: 'Переглянути всі'}];
+  const link = `/${currentLang}${getVideoFromLocation(pathname)}${titleToPathname(category)}/1`;
 
   return (
     <Link
-      to={''}
+      to={link}
       className={style.link}
     >
       <span >{'see all'}</span>
