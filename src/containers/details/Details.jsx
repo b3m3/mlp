c06a9d8d165fd5c4013e53f01546dc3fab8df774
b3m3 <uwm.d.c.a@gmail.com>
2023-01-12 11:20:03 +0200

    Details forwardRef

--- src/containers/details/Details.jsx
+++ src/containers/details/Details.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect, useContext } from 'react';
-import { Link, useLocation, useParams } from 'react-router-dom';
+import { useState, useEffect, useContext, forwardRef } from 'react';
+import { Link, useLocation, useParams, useNavigate } from 'react-router-dom';
 
 import Button from '../../components/ui/button/Button';
 
 import { API_ROOT, API_KEY, API_LANGUAGE, API_BACKGROUND } from '../../constans/api';
 import { getApiResults } from '../../service/getApiResources';
+import { getVideoFromLocation } from '../../utils/functions';
 
 import { Context } from '../../context/context';
 
@@ -12,32 +13,41 @@ import Bd from './img/backdrop.webp';
 
 import style from './details.module.scss';
 
-const Details = () => {
+const Details = forwardRef(({}, ref) => {
   const [results, setResults] = useState(null);
   const [errorApi, setErrorApi] = useState(false);
 
   const { currentLang } = useContext(Context);
   const { pathname } = useLocation();
-  const { id } = useParams();
+  const { videoId } = useParams();
+  const navigate = useNavigate();
+
+  const url = `${API_ROOT}${getVideoFromLocation(pathname)}/${videoId}${API_KEY}${API_LANGUAGE}${currentLang}`;
   
   useEffect(() => {
-  }, []);
+    getApiResults(url, setResults, setErrorApi);
+  }, [url]);
 
   return (
-    <section className={style.wrapp}>
-      {/* {errorApi
+    <section className={style.wrapp} ref={ref}>
+      {errorApi
         ? <h2>Error</h2>
         : <>
             {results &&
               <div className={style.body}>
                 <h2>details</h2>
+
+                <Link 
+                  onClick={() => navigate(-1)}
+                >
+                  <Button left />
+                </Link>
               </div>
             }
           </>
-      } */}
-      <h2>details</h2>
+      }
     </section>
   );
-}
+})
 
 export default Details;
