981bd9077979a2b98eb89119734b317ddcb39dfd
b3m3 <uwm.d.c.a@gmail.com>
2023-01-18 15:00:42 +0200

    Poster

---
+++ src/components/ui/poster/Poster.jsx
@@ -0,0 +1,21 @@
+import { API_POSTER } from '../../../constans/api';
+
+import NoPoster from './img/no-poster.webp';
+
+import style from './poster.module.scss';
+
+const Poster = ({ path }) => {
+  return (
+    <>
+      {path &&
+        <img 
+          src={path ? API_POSTER+path : NoPoster}
+          alt="poster"
+          className={style.img}
+        />
+      }
+    </>
+  );
+}
+
+export default Poster;
