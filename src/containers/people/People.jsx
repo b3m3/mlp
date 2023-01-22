c99398aa7796498be1a7e25e08b6dae904cdac50
b3m3 <uwm.d.c.a@gmail.com>
2023-01-22 10:50:14 +0200

    blank People/Peopleinfo

--- src/containers/people/People.jsx
+++ src/containers/people/People.jsx
@@ -1,24 +1,9 @@
-import { useContext } from 'react';
-
-import Preview from '../../components/preview/Preview';
-import { PEOPLE_POPULAR, API_LANGUAGE } from '../../constans/api';
-
-import { Context } from '../../context/context';
-
 import style from './people.module.scss';
 
 const People = () => {
-  const { currentLang } = useContext(Context);
-
-  const items = [
-    {en: 'Popular', uk: 'Популярні', ru: 'Популярные', url: PEOPLE_POPULAR+API_LANGUAGE+currentLang},
-  ];
-
   return (
-    <section className={style.movies}>
-      {items.map((item, i) => (
-        <Preview key={i} item={item}/>
-      ))}
+    <section className={style.wrapp}>
+      <h2>People</h2>
     </section>
   );
 }
---
+++ src/containers/pepleInfo/PeopleInfo.jsx
@@ -0,0 +1,11 @@
+import style from './people-info.module.scss';
+
+const PeopleInfo = () => {
+  return (
+    <div className={style.wrapp}>
+      <h2>People Info</h2>
+    </div>
+  );
+}
+
+export default PeopleInfo;
