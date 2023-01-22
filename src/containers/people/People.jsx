609497098bf806055f6c1821975657924f3800a2
b3m3 <uwm.d.c.a@gmail.com>
2023-01-22 17:47:03 +0200

    People body

--- src/containers/people/People.jsx
+++ src/containers/people/People.jsx
@@ -1,11 +1,26 @@
+import { useContext } from 'react';
+
+import Preview from '../../components/preview/Preview';
+import { PEOPLE_POPULAR, API_LANGUAGE } from '../../constans/api';
+import { Context } from '../../context/context';
+
 import style from './people.module.scss';
 
 const People = () => {
+  const { currentLang } = useContext(Context);
+
+  const items = [
+    {en: 'Popular', uk: 'Популярні', ru: 'Популярные', url: PEOPLE_POPULAR+API_LANGUAGE+currentLang},
+  ];
+
   return (
     <section className={style.wrapp}>
-      <h2>People</h2>
+      {items.map((item, i) => (
+        <Preview key={i} item={item} actors />
+      ))}
     </section>
   );
 }
 
 export default People;
+
