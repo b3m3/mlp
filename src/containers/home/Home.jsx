3cff57229f6098da3c8a15411b842450b1270f78
b3m3 <uwm.d.c.a@gmail.com>
2023-01-28 23:05:17 +0200

    cr Home items

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,16 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useContext } from 'react';
+
+import { Context } from '../../context/context';
+import Trending from '../../components/trending/Trending';
+
+import { API_ROOT, API_MOVIE, API_TV_SHOWS, API_ACTORS, API_KEY, API_DAY, API_TRENDING, API_LANGUAGE } from '../../constans/api';
 
 import style from './home.module.scss';
 
 const Home = () => {
-  const [results, setResults] = useState(null);
+  const { currentLang } = useContext(Context);
 
-  useEffect(() => {
-  }, []);
+  const items = [
+    {
+      en: 'Trending мovies',
+      uk: 'Фільми у тренді',
+      ru: 'Фильмы в тренде',
+      url: API_ROOT+API_TRENDING+API_MOVIE+API_DAY+API_KEY+API_LANGUAGE+currentLang,
+      type: API_MOVIE,
+    },
+    {
+      en: 'Trending TV Shows',
+      uk: 'Серіали у тренді',
+      ru: 'Сериалы в тренде',
+      url: API_ROOT+API_TRENDING+API_TV_SHOWS+API_DAY+API_KEY+API_LANGUAGE+currentLang,
+      type: API_TV_SHOWS,
+    },
+    // {
+    //   en: 'Trending Actors',
+    //   uk: 'Актори у тренді',
+    //   ru: 'Актеры в тренде',
+    //   url: API_ROOT+API_TRENDING+API_ACTORS+API_DAY+API_KEY+API_LANGUAGE+currentLang,
+    //   type: API_ACTORS,
+    // }
+  ];
 
   return (
-    <section className={style.home}>
-      <h2>Home page</h2>
+    <section className={style.wrapp}>
+      {items.map((item, i) => (
+        <Trending key={i} item={item} />
+      ))}
     </section>
   );
 }
