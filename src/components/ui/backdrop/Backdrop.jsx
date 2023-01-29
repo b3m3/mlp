6ca87864c488e77f0809b53dd01044ce7203d498
b3m3 <uwm.d.c.a@gmail.com>
2023-01-29 23:09:25 +0200

    update Backdrop

--- src/components/ui/backdrop/Backdrop.jsx
+++ src/components/ui/backdrop/Backdrop.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from 'react';
+
 import { API_IMAGE_ORIGINAL } from '../../../constans/api';
 
 import bg from './img/bg.webp';
 
 import style from './backdrop.module.scss';
 
-const Backdrop = ({path}) => {
+const Backdrop = ({ path }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, []);
+
   return (
-    <img 
-      className={style.img} 
-      src={path ? API_IMAGE_ORIGINAL+path : bg}
-      alt={'backdrop'}
-    />
+    <div>
+      {!loaded && 
+        <img 
+          className={style.img} 
+          src={bg} 
+          alt={'backdrop'}
+        />
+      }
+
+      <img
+        onLoad={() => setLoaded(true)}
+        className={style.img}
+        src={path ? API_IMAGE_ORIGINAL+path : bg}
+        alt={'backdrop'}
+      />
+    </div>
   );
 }
 
