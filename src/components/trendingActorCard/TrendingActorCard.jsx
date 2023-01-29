dd0bdf99818b71a45aeb8d85d24f6b3e1eb42942
b3m3 <uwm.d.c.a@gmail.com>
2023-01-29 11:25:51 +0200

    TrendingActorCard

---
+++ src/components/trendingActorCard/TrendingActorCard.jsx
@@ -0,0 +1,21 @@
+import Backdrop from '../ui/backdrop/Backdrop';
+import GetInfoBtn from '../ui/getInfoBtn/GetInfoBtn';
+
+import style from './trending-actor-card.module.scss';
+
+const TrendingActorCard = ({ id, type, original_name, profile_path }) => {
+  return (
+    <div className={style.wrapp}>
+      <Backdrop path={profile_path} />
+
+      <div className={style.body}>
+        <div className={style.content}>
+          <h2>{original_name && original_name}</h2>
+          <GetInfoBtn id={id} type={type} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrendingActorCard;
