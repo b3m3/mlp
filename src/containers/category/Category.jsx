3f7b0e37ec56274e4ff8fdf4793a23e2b7fd394c
b3m3 <uwm.d.c.a@gmail.com>
2023-01-30 16:31:57 +0200

    update category for search

--- src/containers/category/Category.jsx
+++ src/containers/category/Category.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState, useContext } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 
-import { getApiResults, getApiTotalPages } from '../../service/getApiResources';
+import { getApiResources, getApiResults, getApiTotalPages } from '../../service/getApiResources';
 import { translateCategoryTitle } from '../../utils/functions';
-import { API_ROOT, API_KEY, API_PAGE, API_LANGUAGE } from '../../constans/api';
+import { API_ROOT, API_KEY, API_PAGE, API_LANGUAGE, API_QUERY } from '../../constans/api';
 
 import PageNavigation from '../../components/ui/pageNavigation/PageNavigation';
 import Error from '../../components/error/Error';
@@ -12,6 +12,7 @@ import { Context } from '../../context/context';
 
 import VideoCard from '../../components/videoCard/VideoCard';
 import ActorCard from '../../components/actorCard/ActorCard';
+import SearchError from '../../components/searchError/SearchError';
 import ShimmerActorCard from '../../components/ui/shimmers/shimmerActorCard/ShimmerActorCard';
 import ShimmerVideoCard from '../../components/ui/shimmers/shimmerVideoCard/ShimmerVideoCard';
 
@@ -20,10 +21,11 @@ import style from './category.module.scss';
 const Category = () => {
   const [results, setResults] = useState(null);
   const [errorApi, setErrorApi] = useState(false);
+  const [noSearchResults, setNoSearchResults] = useState(false);
   const [totalPages, setTotalPages] = useState(null);
 
   const { currentLang } = useContext(Context);
-  const { type, category, page } = useParams();
+  const { type, category, page, id } = useParams();
   const { pathname } = useLocation();
 
   const titles = [
@@ -35,19 +37,37 @@ const Category = () => {
     {airing_today: [{en: 'Airing today', uk: 'Сьогодні в ефірі', ru: 'Сегодня в эфире'}]}
   ];
 
-  const url = API_ROOT+'/'+type+'/'+category+API_KEY+API_LANGUAGE+currentLang+API_PAGE+page;
+  const isSearch = category === 'search';
+
+  const url = isSearch
+    ? API_ROOT+'/'+category+'/'+type+API_KEY+API_LANGUAGE+currentLang+API_QUERY+id+API_PAGE+page
+    : API_ROOT+'/'+type+'/'+category+API_KEY+API_LANGUAGE+currentLang+API_PAGE+page;
 
   useEffect(() => {
     setResults(null);
+    setNoSearchResults(false);
     window.scrollTo(0, 0);
-    getApiResults(url, setResults, setErrorApi);
     getApiTotalPages(url, setTotalPages);
+
+    (async() => {
+      const res = await getApiResources(url);
+
+      if (res) {
+        setResults(res);
+      } else {
+        setErrorApi(true);
+      }
+
+      if (isSearch && res && res.results.length < 1) {
+        setNoSearchResults(true);
+      }
+    })();
   }, [currentLang, pathname, url]);
 
   return (
     <section className={style.category}>
       <h2>
-        {translateCategoryTitle(titles, category, currentLang)}
+        {isSearch ? id : translateCategoryTitle(titles, category, currentLang)}
       </h2>
 
       {errorApi
@@ -74,6 +94,8 @@ const Category = () => {
                 </>
             }
           </div>}
+
+      {noSearchResults && <SearchError value={id} />}
 
       <PageNavigation 
         totalPages={totalPages && totalPages > 500 ? 500 : totalPages}
