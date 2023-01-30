689b15cbbfba98ccc3b5832f9d7caec01f212640
b3m3 <uwm.d.c.a@gmail.com>
2023-01-30 10:24:28 +0200

    fix Background

--- src/components/ui/background/Background.jsx
+++ src/components/ui/background/Background.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { API_IMAGE_ORIGINAL } from '../../../constans/api';
 import { getTypeFromLocation } from '../../../utils/functions';
 
-import BackdropImg from './img/backdrop.webp';
+import bgMask from './img/bg-mask.webp';
 
 import style from './background.module.scss';
 
@@ -15,8 +15,8 @@ const Background = ({ path }) => {
   return (
     <div className={style.wrapp}>
       <img 
-        src={path ? API_IMAGE_ORIGINAL+path : BackdropImg}
-        alt={'backdrop'} 
+        src={path ? API_IMAGE_ORIGINAL+path : bgMask}
+        alt={'background'}
       />
       <span className={isPerson ? style.blur : style.gradient} />
     </div>
