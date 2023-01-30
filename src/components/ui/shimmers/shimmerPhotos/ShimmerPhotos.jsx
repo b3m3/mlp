2ab216b8c5758afc2fbdb590387ba9424fd35870
b3m3 <uwm.d.c.a@gmail.com>
2023-01-30 09:59:59 +0200

    ShimmerPhotos

---
+++ src/components/ui/shimmers/shimmerPhotos/ShimmerPhotos.jsx
@@ -0,0 +1,11 @@
+import style from './shimmer-photos.module.scss';
+
+const ShimmerPhotos = () => {
+  return (
+    <div className={style.wrapp}>
+      <i/><i/><i/>
+    </div>
+  );
+}
+
+export default ShimmerPhotos;
