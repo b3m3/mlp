2955ffc9b573786f98e922c1b51687a44678c0a4
b3m3 <uwm.d.c.a@gmail.com>
2023-02-06 15:27:12 +0200

    Favorites page

--- src/containers/favorites/Favorites.jsx
+++ src/containers/favorites/Favorites.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 
 import { Context } from '../../context/context';
 import VideoCard from '../../components/videoCard/VideoCard';
@@ -6,52 +6,26 @@ import VideoCard from '../../components/videoCard/VideoCard';
 import style from './favorites.module.scss';
 
 const Favorites = () => {
-  // const [favoriteItems, setFavoriteItems] = useState([]);
   const { currentLang, favorites } = useContext(Context);
 
   const titles = [{en: 'Favorites', uk: 'Вибране', ru: 'Избранное'}];
-
-  // useEffect(() => {
-  //   if (favorites) {
-  //     for (const key in favorites) {
-  //       setFavoriteItems(c => [...c, favorites[key]]);
-  //     }
-  //   }
-  // }, []);
-
-  // const df = () => {
-  //   if (favorites) {
-  //     for (const key in favorites) {
-  //       const {id, poster_path, title, name} = favorites[key];
-  
-  //       return(
-  //         <VideoCard 
-  //           id={id}
-  //           poster_path={poster_path}
-  //           title={title}
-  //           name={name}
-  //         />
-  //       )
-  //     }
-  //   }
-  // };
 
   return (
     <section className={style.favorites}>
       <h2>{titles.map(t => t[currentLang])}</h2>
 
       <ul>
-        {/* {df()} */}
-        {/* {favoriteItems && favoriteItems.map(({ id, poster_path, title, name }) => (
+        {favorites && favorites.map(({id, poster_path, title, name, type}) => (
           <li key={id}>
             <VideoCard 
               id={id}
               poster_path={poster_path}
               title={title}
               name={name}
+              type={type}
             />
           </li>
-        ))} */}
+        ))}
       </ul>
     </section>
   );
