9eff9159a76e02a721abd3df2950168e9010365e
b3m3 <uwm.d.c.a@gmail.com>
2023-02-09 11:30:12 +0200

    PhotoGallery

---
+++ src/components/infoActor/photoGallery/PhotoGallery.jsx
@@ -0,0 +1,54 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from "swiper";
+
+import Backdrop from '../../ui/backdrop/Backdrop';
+import Button from '../../ui/button/Button';
+
+import { IoCloseCircleSharp } from 'react-icons/io5';
+
+import style from './photo-gallery.module.scss';
+import 'swiper/css';
+import "swiper/css/navigation";
+
+const PhotoGallery = ({ results, initial, setShowGallery }) => {
+  return (
+    <div className={style.wrapp}>
+      <Swiper
+        className={style.swiper}
+        slidesPerView={1}
+        initialSlide={initial && initial}
+        navigation={{
+          prevEl: '.ph-gal-prev',
+          nextEl: '.ph-gal-next'
+        }}
+        loop={true}
+        modules={[Navigation]}
+      >
+        {results && results.profiles.map(({ file_path }, i) => (
+          <SwiperSlide key={i}>
+            <Backdrop path={file_path} actor ligth />
+          </SwiperSlide>
+        ))}
+
+      </Swiper>
+
+      <div className={style.navigate}>
+        <span className='ph-gal-prev'>
+          <Button left />
+        </span>
+        <span className='ph-gal-next'>
+          <Button />
+        </span>
+      </div>
+
+      <span
+        className={style.close}
+        onClick={() => setShowGallery(false)}
+      >
+        <IoCloseCircleSharp />
+      </span>
+    </div>
+  );
+}
+
+export default PhotoGallery;
--- src/components/infoActor/photos/Photos.jsx
+++ src/components/infoActor/photos/Photos.jsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 
+import ShimmerPhotos from '../../ui/shimmers/shimmerPhotos/ShimmerPhotos';
+import Backdrop from '../../ui/backdrop/Backdrop';
+import PhotoGallery from '../photoGallery/PhotoGallery';
 import { getApiResources } from '../../../service/getApiResources';
 import { getTypeFromLocation } from '../../../utils/functions';
 import { API_ROOT, API_KEY, API_ACTORS_IMAGES, API_IMAGE_ORIGINAL } from '../../../constans/api';
-import ShimmerPhotos from '../../ui/shimmers/shimmerPhotos/ShimmerPhotos';
-import Backdrop from '../../ui/backdrop/Backdrop';
 
 import style from './photos.module.scss';
 
 const Photos = () => {
   const [results, setResults] = useState(null);
+  const [showGallery, setShowGallery] = useState(false);
+  const [initialSlide, setInitialSlide] = useState(null);
 
   const { id } = useParams();
   const { pathname } = useLocation();
@@ -26,18 +29,31 @@ const Photos = () => {
 
   return (
     <>
-      {results
-        ? <ul className={style.list}>
-            {results.profiles.map(({file_path}, i) => (
-              <li key={file_path}>
-                <a href={API_IMAGE_ORIGINAL+file_path} target={'_blank'} rel="noreferrer">
-                  <Backdrop path={file_path} actor />
-                </a>
-              </li>
-            ))}
-          </ul>
-        : <ShimmerPhotos />
-      }
+      {showGallery
+        ? <PhotoGallery 
+            results={results} 
+            initial={initialSlide}
+            setShowGallery={setShowGallery}
+          />
+        : <>
+            {results
+              ? <ul className={style.list}>
+                  {results.profiles.map(({file_path}, i) => (
+                    <li 
+                      key={i}
+                      onClick={() => {
+                        setShowGallery(true);
+                        setInitialSlide(i);
+                      }}
+                    >
+                      <Backdrop path={file_path} actor light />
+                    </li>
+                  ))}
+                </ul>
+              : <ShimmerPhotos />
+            }
+          </>
+        }
     </>
   );
 }
