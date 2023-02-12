8c3186c49c717f024dc41deb7655e46ecddfcfb2
b3m3 <uwm.d.c.a@gmail.com>
2023-02-12 16:56:53 +0200

    fix Favorites -> del last favorite item in arr

--- src/containers/favorites/Favorites.jsx
+++ src/containers/favorites/Favorites.jsx
@@ -1,21 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 
 import VideoCard from '../../components/videoCard/VideoCard';
 import EmptyPage from '../../components/errors/emptyPage/EmptyPage';
 import { favoritesTitles } from '../../constans/titles';
+import { FAVORITE_KEY } from '../../constans/localStorage';
 import { getTitleLang } from '../../utils/functions';
+import { removeItemFromLocalStorage } from '../../utils/localStorage';
 import { Context } from '../../context/context';
 
 import style from './favorites.module.scss';
 
 const Favorites = () => {
-  const { currentLang, favorites } = useContext(Context);
+  const { currentLang, favorites, setFavorites } = useContext(Context);
+
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const onClearArr = (e) => {
+      if (e.target && !favorites.length) {
+        setFavorites([]);
+        removeItemFromLocalStorage(FAVORITE_KEY);
+      }
+    }
+
+    document.addEventListener('click', onClearArr);
+    return() => document.removeEventListener('click', onClearArr);
+  }, [favorites, setFavorites]);
 
   return (
-    <section className={style.favorites}>
+    <section className={style.favorites} ref={ref}>
       <h2>{getTitleLang(favoritesTitles, currentLang)}</h2>
 
-      {favorites && favorites.length > 0
+      {favorites.length > 0
         ? <ul>
             {favorites.map(({id, poster_path, title, name, type}) => (
               <li key={id}>
