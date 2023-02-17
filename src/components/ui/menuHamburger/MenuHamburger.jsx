c1eaf8aefe59f4409443f7e7762cdb90ced7b604
b3m3 <uwm.d.c.a@gmail.com>
2023-02-17 16:05:12 +0200

    update MenuHamburger

--- src/components/ui/menuHamburger/MenuHamburger.jsx
+++ src/components/ui/menuHamburger/MenuHamburger.jsx
@@ -1,32 +1,38 @@
 import { useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleMenu, closeMenu } from '../../../store/slices/menuSlices';
 
 import { Context } from '../../../context/context';
 
 import style from './hamburger.module.scss';
 
 const MenuHamburger = () => {
-
   const [isActive, setIsActive] = useState(true);
 
+  const menuState = useSelector(state => state.menu.menuState)
+  const dispatch = useDispatch();
+
   const { pathname } = useLocation();
-  const { menuActive, setMenuActive, refInfoVideo, refInfoActor } = useContext(Context);
+  const { refInfoVideo, refInfoActor } = useContext(Context);
 
   useEffect(() => {
     if (refInfoVideo.current || refInfoActor.current) {
       setIsActive(false);
-      setMenuActive(false);
+      dispatch(closeMenu())
     } else {
-      setIsActive(true)
+      setIsActive(true);
     }
-  }, [pathname, setMenuActive, refInfoVideo, refInfoActor]);
+  }, [pathname, isActive, dispatch, refInfoVideo, refInfoActor]);
+
+  const className = `${style.hamburger} ${menuState && style.active}`;
 
   return (
     <>
-      {isActive &&      
+      {isActive &&
         <button 
-          className={menuActive ? `${style.active} ${style.hamburger}` : style.hamburger}
-          onClick={() => setMenuActive(a => !a)}
+          className={className}
+          onClick={() => dispatch(toggleMenu())}
         >
           <i></i>
           <i></i>
