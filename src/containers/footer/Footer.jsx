965d6c643ff0dfc2c6fcf3e3b13ea7a9b1c110bc
b3m3 <uwm.d.c.a@gmail.com>
2023-02-21 17:04:10 +0200

    update Sidebar/Footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,57 +1,33 @@
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import MenuLinks from '../../components/ordinary/menuLinks/MenuLinks';
+import Navbar from '../../components/ordinary/navbar/Navbar';
 
-import tmdb from './img/tmdb.svg';
+import LogoTmdb from '../../components/ui/logoTmdb/LogoTmdb';
 
 import style from './footer.module.scss';
-import { useMemo } from 'react';
+
+const links = [
+  {link: 'https://www.themoviedb.org/', name: 'TMDb'},
+  {link: 'https://twitter.com/themoviedb', name: 'TMDb Twitter'}
+];
 
 const Footer = () => {
-  const language = useSelector(state => state.language.language);
-
-  const tmdbLinks = useMemo(() => [
-    {link: 'https://www.themoviedb.org/', name: 'TMDb'},
-    {link: 'https://twitter.com/themoviedb',name: 'TMDb Twitter'}
-  ], []);
-
-  const navigationLinks = useMemo(() => [
-    {path: `/${language}`, name: 'Home'},
-    {path: `/${language}/movie`, name: 'Movies'},
-    {path: `/${language}/tv`, name: 'TV Shows'},
-    {path: `/${language}/person`, name: 'Actors'},
-    {path: `/${language}/favorites`, name: 'Favorites'},
-  ], [language]);
-
   return (
     <footer className={style.wrapp}>
       <div className="container">
         <div className={style.body}>
+
           <div className={style.col}>
             <h5>Navigation</h5>
-            <ul>
-              {navigationLinks.map(({path, name}, i) => (
-                <li key={i}>
-                  <Link to={path}>{name}</Link>
-                </li>
-              ))}
-            </ul>
+            <Navbar />
           </div>
 
           <div className={style.col}>
             <h5>About</h5>
-            <ul>
-              {tmdbLinks.map(({link, name}, i) => (
-                <li key={i}>
-                  <a href={link} target="_blank" rel="noreferrer">{name}</a>
-                </li>
-              ))}
-            </ul>
+            <MenuLinks links={links} />
           </div>
 
           <div className={style.col}>
-            <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
-              <img src={tmdb} alt="TMDb" />
-            </a>
+            <LogoTmdb />
           </div>
         </div>
       </div>
--- src/containers/sidebar/Sidebar.jsx
+++ src/containers/sidebar/Sidebar.jsx
@@ -1,47 +1,22 @@
-import { NavLink, useLocation } from 'react-router-dom';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { onCloseInfo } from '../../store/slices/infoSlice';
 
-import { AiFillHome } from 'react-icons/ai';
-import { MdLocalMovies } from 'react-icons/md';
-import { RiMovie2Fill } from 'react-icons/ri';
-import { MdFavorite } from 'react-icons/md';
-import { BsFillPeopleFill } from 'react-icons/bs';
-
 import style from './sidebar.module.scss';
-
-const titles = [
-  {en: 'Home', uk: 'Домашня', ru: 'Домашняя'},
-  {en: 'Movies', uk: 'Фільми', ru: 'Фильмы'},
-  {en: 'TV Shows', uk: 'Серіали', ru: 'Сериалы'},
-  {en: 'Actors', uk: 'Актори', ru: 'Актеры'},
-  {en: 'Favorites', uk: 'Обране', ru: 'Избранное'}
-];
+import Navbar from '../../components/ordinary/navbar/Navbar';
 
 const Sidebar = () => {
   const [isActive, setIsActive] = useState(false);
 
   const menuState = useSelector(state => state.menu.menuState);
-  const favoriteList = useSelector(state => state.favorite.favoritesList);
-  const language = useSelector(state => state.language.language);
   const infoState = useSelector(state => state.info.infoState);
   const dispatch = useDispatch();
 
   const { pathname } = useLocation();
 
-  const links = useMemo(() => [
-    {icon: <AiFillHome />, path: `/${language}`, end: true},
-    {icon: <MdLocalMovies />, path: `/${language}/movie`},
-    {icon: <RiMovie2Fill />, path: `/${language}/tv`},
-    {icon: <BsFillPeopleFill />, path: `/${language}/person`},
-    {icon: <MdFavorite />, path: `/${language}/favorites`}
-  ], [language]);
-
-  const classNameNav = `${style.navbar} ${menuState && style.active} ${isActive && style.hidden}`;
-
-  const linkName = useCallback((i) => titles[i][language], [language]);
+  const className = `${style.wrapp} ${menuState && style.active} ${isActive && style.hidden}`;
 
   useEffect(() => {
     dispatch(onCloseInfo());
@@ -49,29 +24,8 @@ const Sidebar = () => {
   }, [pathname, infoState, dispatch]);
   
   return (
-    <aside>
-      <nav className={classNameNav}>
-        <ul>
-          {links.map((link, i) => (
-            <li key={i}>
-              <NavLink 
-                to={link.path} 
-                end={link.end && link.end} 
-                className={({isActive}) => isActive ? style.active : ''}
-              >
-                {link.icon}
-                <span>{linkName(i)}</span>
-              </NavLink>
-
-              {links.length -1 === i && favoriteList && favoriteList.length > 0 &&
-                <i className={menuState ? style.open : ''}>
-                  {favoriteList.length}
-                </i>
-              }
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <aside className={className}>
+      <Navbar sidebar />
     </aside>
   );
 }
