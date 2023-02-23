1e7f1520f9c7b192df132db5a0d8bb9e6ee1b6e2
b3m3 <uwm.d.c.a@gmail.com>
2023-02-23 23:30:46 +0200

    update Poster

--- src/components/ui/poster/Poster.jsx
+++ src/components/ui/poster/Poster.jsx
@@ -1,18 +1,26 @@
 import { API_POSTER } from '../../../constants/api';
+import { useLoadingImage } from '../../../hooks/useLoadingImage';
 
-import NoPoster from './img/no-poster.webp';
+import NoPoster from './img/not-image.webp';
 
 import style from './poster.module.scss';
 
 const Poster = ({ path }) => {
   const src = path ? path.indexOf('http') === 1 ? path.slice(1) : API_POSTER+path : NoPoster;
 
+  const { Mask, setLoaded } = useLoadingImage();
+
   return (
-    <img 
-      src={src}
-      alt="poster"
-      className={style.img}
-    />
+    <>
+      <Mask />
+
+      <img
+        onLoad={() => setLoaded(true)}
+        src={src}
+        alt="poster"
+        className={style.img}
+      />
+    </>
   );
 }
 
