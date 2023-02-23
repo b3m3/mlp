dada4fc98d78010c37753b2132c357d1111d09a6
b3m3 <uwm.d.c.a@gmail.com>
2023-02-23 14:44:35 +0200

    CardsList.jsx

---
+++ src/components/smart/search/cardsList/CardsList.jsx
@@ -0,0 +1,38 @@
+import { useCallback } from 'react';
+
+import Card from '../card/Card';
+
+import style from './cards-list.module.scss';
+
+const CardsList = ({
+  results, isActors, type, setResults, setGenresSelected,
+  setRatings, setYears, setSortBy, setInputFocus, setInputValue
+}) => {
+  const clearHandler = useCallback((e) => {
+    if (e.target && e.target.tagName.toLowerCase() === 'img') {
+      setResults(null);
+      setGenresSelected([]);
+      setRatings([]);
+      setYears([]);
+      setSortBy([]);
+      setInputFocus(false);
+      setInputValue('');
+    }
+  }, [setResults, setGenresSelected, setRatings, setSortBy, setYears, setInputFocus, setInputValue]);
+
+  return (
+    <ul className={style.wrapp}>
+      {results.map(({id, poster_path, profile_path}) => (
+        <li key={id} onClick={e => clearHandler(e)}>
+          <Card
+            id={id}
+            poster_path={isActors ? profile_path : poster_path}
+            type={type}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CardsList;
