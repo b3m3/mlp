a57d8c7ff3185fd8507369506c51df7d5c3be4f8
b3m3 <uwm.d.c.a@gmail.com>
2023-02-23 11:47:41 +0200

    update GenreButton.jsx

--- src/components/smart/search/genreButton/GenreButton.jsx
+++ src/components/smart/search/genreButton/GenreButton.jsx
@@ -1,27 +1,39 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import style from './genre-button.module.scss';
 
-const GenreButton = ({ name, id, indexSectionBtn, activeBtn, setGenresSelected }) => {
+const GenreButton = ({ name, id, indexSectionBtn, activeBtn, genresSelected, setGenresSelected }) => {
   const [isActive, setIsActive] = useState(false);
+
+  const activeSelected = useCallback(() => {
+    return genresSelected.filter(genre => genre.id === id && setIsActive(true));
+  }, [genresSelected, id]);
+
+  const handleRemove = useCallback(() => {
+    return setGenresSelected(a => a.filter(el => el.name !== name));
+  }, [setGenresSelected, name]);
+
+  const handleAdd = useCallback(() => {
+    return setGenresSelected(c => [...c, {id, name}]);
+  }, [setGenresSelected, id, name]); 
 
   useEffect(() => {
     setIsActive(false);
-  }, [indexSectionBtn]);
+    activeSelected();
+  }, [indexSectionBtn, activeSelected]);
+
+  const nameBtn = name[0].toUpperCase() + name.slice(1);
 
   return (
     <button
       onClick={() => {
-        isActive 
-          ? setGenresSelected(a => a.filter(el => el.name !== name))
-          : setGenresSelected(c => [...c, {id, name}]);
-
+        isActive ? handleRemove() : handleAdd()
         setIsActive(a => !a);
       }}
       className={style.btn}
       style={isActive ? activeBtn : null}
     >
-      {name[0].toUpperCase() + name.slice(1)}
+      {nameBtn}
     </button>
   );
 }
