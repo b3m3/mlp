cc8093eac77ec3c28888c977c11dd49b12aa3d82
b3m3 <uwm.d.c.a@gmail.com>
2023-02-24 13:36:13 +0200

    tvSeasonsButton.jsx

---
+++ src/components/ui/tvSeasonsButton/TvSeasonsButton.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { API_SEASON, API_TV_SHOWS } from '../../../constants/api';
+import { getTitleLang } from '../../../utils/functions';
+
+import { MdPlayArrow } from 'react-icons/md';
+
+import style from './tv-seasons-button.module.scss';
+
+const titles = [
+  {en: 'Seasons', uk: 'Сезони', ru: 'Сезоны'}
+]
+
+const TvSeasonsButton = ({id}) => {
+  const language = useSelector(state => state.language.language);
+  const title = getTitleLang(titles, language);
+
+  const link = '/'+language+API_TV_SHOWS+'/'+id+API_SEASON;
+
+  return (
+    <Link 
+      className={style.btn}
+      to={link}
+    >
+      <span>{title}</span>
+      <MdPlayArrow />
+    </Link>
+  );
+}
+
+export default TvSeasonsButton;
