908e4b05c13c57c6e4192f1c0c1e2dfe964d8254
b3m3 <uwm.d.c.a@gmail.com>
2023-02-25 15:27:26 +0200

    InfoSeasons.jsx

---
+++ src/containers/infoSeasons/InfoSeasons.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import { onActiveInfo } from '../../store/slices/infoSlice';
+
+import Back from '../../components/ui/back/Back';
+import Seasons from '../../components/smart/seasons/Seasons';
+import SeasonNumber from '../../components/smart/seasonNumber/SeasonNumber';
+import Background from '../../components/ui/background/Background';
+
+import style from './info-seasons.module.scss';
+
+const InfoSeasons = () => {
+  const [background, setBackground] = useState(null);
+  const [episodeNumber, setEpisodeNumber] = useState(0);
+
+  const language = useSelector(state => state.language.language);
+  const infoState = useSelector(state => state.info.infoState);
+  const dispatch = useDispatch();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(onActiveInfo());
+  }, [dispatch, infoState]);
+
+  return (
+    <section className={style.wrapp}>
+      <Background path={background} />
+      <Back path={-1} />
+
+      <div className={style.body}>
+        <div className={style.col}>
+          <SeasonNumber 
+            id={id}
+            language={language}
+            episodeNumber={episodeNumber}
+            setEpisodeNumber={setEpisodeNumber}
+          />
+        </div>
+        <div className={style.col}>
+          <Seasons 
+            id={id}
+            language={language}
+            setBackground={setBackground}
+            setEpisodeNumber={setEpisodeNumber}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default InfoSeasons;
