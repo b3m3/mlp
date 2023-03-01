288a06ba416162f67ab6d9c54fdffae3b25779ca
b3m3 <uwm.d.c.a@gmail.com>
2023-03-01 16:52:18 +0200

    upd Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,17 +1,12 @@
-import { useSelector } from 'react-redux';
-
 import Search from '../../components/smart/search/search/Search';
 import Logo from '../../components/ui/logo/Logo';
 import Language from '../../components/smart/language/Language';
 import MenuHamburger from '../../components/ordinary/menuHamburger/MenuHamburger';
-import AuthButton from '../../components/ui/authButton/AuthButton';
-import AuthModal from '../../components/ordinary/authModal/AuthModal';
+import Auth from '../../components/smart/auth/Auth';
 
 import style from './header.module.scss';
 
 const Header = () => {
-  const authModal = useSelector(s => s.modal.modal);
-
   return (
     <div className="container">
       <header className={style.header}>
@@ -19,13 +14,11 @@ const Header = () => {
         <Search />
 
         <div className={style.box}>
-          <AuthButton />
+          <Auth />
           <Language />
           <MenuHamburger />
         </div>
       </header>
-
-      {authModal && <AuthModal />}
     </div>
   );
 }
