7eab7ce827ac8bbb88bf98a61464d8bb097d5184
b3m3 <uwm.d.c.a@gmail.com>
2023-03-02 10:26:15 +0200

    upd Auth

--- src/components/smart/auth/Auth.jsx
+++ src/components/smart/auth/Auth.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import secureLocalStorage from "react-secure-storage";
+import axios from 'axios';
 
 import { handleUser } from '../../../store/slices/userSlice';
 import { openModal } from '../../../store/slices/authModalSlice';
@@ -9,14 +10,15 @@ import AuthModal from '../../ordinary/authModal/AuthModal';
 import LogoutButton from '../../ui/logoutButton/LogoutButton';
 import LoginButton from '../../ui/loginButton/LoginButton';
 import Poster from '../../ui/poster/Poster';
+import Spinner from '../../ui/spinner/Spinner';
 
 import { ACCOUNT } from '../../../constants/api';
 import { SESSION_ID_KEY } from '../../../constants/localStorage';
 
 import style from './auth.module.scss';
-import axios from 'axios';
 
 const Auth = () => {
+  const [loading, setLoading] = useState(false);
   const [avatarPath, setAvatarPath] = useState(null);
   const [username, setUsername] = useState(null);
 
@@ -38,20 +40,22 @@ const Auth = () => {
   const getAuthState = useCallback(() => {
     if (session_id) return dispatch(handleUser(true));
     return dispatch(handleUser(false));
-  }, [dispatch, session_id])
+  }, [dispatch, session_id]);
 
   useEffect(() => {
     getAuthState();
   }, [getAuthState, user]);
 
   useEffect(() => {
-    if (session_id && session_id.length > 30) {
-      console.log(session_id);
+    if (session_id && session_id !== null) {
+      setLoading(true);
+
       axios.get(ACCOUNT+session_id)
         .then(data => {
           setUsername(data.data.username);
           setAvatarPath(data.data.avatar.tmdb.avatar_path);
         })
+        .finally(() => setLoading(false));
     }
   }, [session_id]);
 
@@ -59,12 +63,15 @@ const Auth = () => {
     <div className={style.wrapp}>
       {session_id
         ? <>
-            {avatarPath &&
-              <div className={style.poster}>
-                <Poster path={avatarPath} />
-              </div>
-            }
-            <LogoutButton handleLogout={handleLogout} username={username} />
+          {loading
+            ? <Spinner/>
+            : <>            
+                <div className={style.poster}>
+                  <Poster path={avatarPath} />
+                </div>
+                <LogoutButton handleLogout={handleLogout} username={username} />
+              </>
+          }
           </>
         : <LoginButton handModal={handModal} />}
 
