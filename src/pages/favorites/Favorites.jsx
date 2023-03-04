75c3f5cf0bb71a35a1619b7660dd9fa83c969148
b3m3 <uwm.d.c.a@gmail.com>
2023-03-04 16:25:04 +0200

    Favorites body

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,20 +1,39 @@
 import { useSelector } from 'react-redux';
 
+import FavoriteList from '../../components/ordinary/favoriteList/FavoriteList';
 import { getTitleLang } from '../../utils/functions';
 
 import style from './favorites.module.scss';
 
-const favoritesTitles = [
-  {en: 'Favorites', uk: 'Обране', ru: 'Избранное'}
+const titlesMovie = [
+  {en: 'Favorite Movies', uk: 'Обрані фільми', ru: 'Избранные фильмы'}
+];
+
+const titlesTv = [
+  {en: 'Favorite tv shows', uk: 'Обрані серіали', ru: 'Избранные сериалы'}
 ];
 
 const Favorites = () => {
   const language = useSelector(state => state.language.language);
+  const favoriteMovies = useSelector(state => state.favorite.favoriteMovies);
+  const favoriteTv = useSelector(state => state.favorite.favoriteTv);
+
+  const titleMovie = getTitleLang(titlesMovie, language)
+  const titleTv = getTitleLang(titlesTv, language)
 
   return (
-    <section className={style.favorites}>
-      <h2>{getTitleLang(favoritesTitles, language)}</h2>
+    <section className={style.wrapp}>
+      <FavoriteList 
+        results={favoriteMovies} 
+        type={'/movie'}
+        title={titleMovie}
+      />
 
+      <FavoriteList 
+        results={favoriteTv}
+        type={'/tv'}
+        title={titleTv}
+      />
     </section>
   );
 }
