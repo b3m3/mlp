95e1e631562aff6671ceeb4ed3fa334bdc65edb7
b3m3 <uwm.d.c.a@gmail.com>
2023-03-06 16:40:14 +0200

    show AuthModal in favorite

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,10 +1,13 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { openModal } from '../../store/slices/authModalSlice';
 
 import EmptyPage from '../../components/ui/errors/emptyPage/EmptyPage';
 import FavoriteList from '../../components/ordinary/favoriteList/FavoriteList';
 import { getTitleLang } from '../../utils/functions';
 
 import style from './favorites.module.scss';
+import { useEffect } from 'react';
 
 const titlesMovie = [
   {en: 'Favorite Movies', uk: 'Обрані фільми', ru: 'Избранные фильмы'}
@@ -18,12 +21,21 @@ const Favorites = () => {
   const language = useSelector(state => state.language.language);
   const favoriteMovies = useSelector(state => state.favorite.favoriteMovies);
   const favoriteTv = useSelector(state => state.favorite.favoriteTv);
+  const auth = useSelector(state => state.auth.auth);
+
+  const dispatch = useDispatch();
 
   const titleMovie = getTitleLang(titlesMovie, language);
   const titleTv = getTitleLang(titlesTv, language);
 
   const isFavoriteMovies = favoriteMovies && favoriteMovies.length === 0;
   const isFavoriteTv = favoriteTv && favoriteTv.length === 0;
+
+  useEffect(() => {
+    if (!auth) {
+      dispatch(openModal());
+    }
+  }, [auth, dispatch])
 
   return (
     <section className={style.wrapp}>
